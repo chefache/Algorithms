74b9b6518f92184e1ed39df23f80294d2e218239
Stefan Koev <stefan.m.koev@gmail.com>
2020-02-26 22:24:47 +0200

    Add files via upload

---
+++ Recursion/Recursive Drawing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Recursive_Drawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int figureSize = int.Parse(Console.ReadLine());
+            Print(figureSize);
+            
+        }
+
+        static void Print( int index)
+        {
+            if (index == 0)
+            {
+                return;
+            }
+            Console.WriteLine(new string('*', index));
+
+            Print(index - 1);
+
+            Console.WriteLine(new string('#', index));
+        }
+    }
+}
