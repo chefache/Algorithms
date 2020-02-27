56a388eadfddd1c4915504dbc6aa6beb912acc0d
Stefan Koev <stefan.m.koev@gmail.com>
2020-02-27 20:55:49 +0200

    Add files via upload

---
+++ Recursion/Generating 0-1 Vectors/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Generating_0_1_Vectors
+{
+    class Program
+    {
+        static void Main()
+        {
+            var arr = new int[8];
+            Generate(0, arr);
+        }
+
+        static void Generate(int index, int[] arr)
+        {
+            if (index == arr.Length)
+            {
+                Console.WriteLine(string.Join(" ", arr));
+            }
+            else
+            {
+                for (int i = 0; i <= 1; i++)
+                {
+                    arr[index] = i;
+                    Generate(index + 1, arr);
+                }
+            }
+        }
+    }
+}
