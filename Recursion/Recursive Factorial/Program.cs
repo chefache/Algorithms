7f410cb6eccc722df4fb3fe466f61f2417ce43ac
Stefan Koev <stefan.m.koev@gmail.com>
2020-02-27 20:56:42 +0200

    Add files via upload

---
+++ Recursion/Recursive Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Recursive_Factorial
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            long result = Factorial(number);
+
+            Console.WriteLine(result);
+        }
+
+        static long Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n + Factorial(n - 1);
+        }
+    }
+}
