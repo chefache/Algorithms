cded4e67f5c45cbc0b47a2850a165191bb0bbb75
Stefan Koev <stefan.m.koev@gmail.com>
2020-02-27 20:56:13 +0200

    Add files via upload

---
+++ Recursion/Recursive Array Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Recursive_Array_Sum
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = new[] { 1, 2, 3, 4, 5 };
+            var result = Sum(numbers, 0);
+            Console.WriteLine(result);
+        }
+
+        static int Sum(int[] arr, int index)
+        {
+            if (index == arr.Length)
+            {
+                return 0;
+            }
+
+            Console.WriteLine("Before recursion " + index);
+
+            var currentSum = arr[index] + Sum(arr, index + 1);
+
+            Console.WriteLine("After recursion " + index);
+
+
+            return currentSum;
+        }
+    }
+}
